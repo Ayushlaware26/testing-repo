4dcfb344509ae8c8eb91fbf09f1d0d1f82b3d09e
ayush <ayushlaware@gmail.com>
2025-03-23 04:30:05 +0530

    add this to pr

--- src/app/name/page.tsx
+++ src/app/name/page.tsx
@@ -4,6 +4,11 @@
             <div>
                 <h1>Enter your Name</h1>
                 <h1>this is pr1</h1>
+
+                <h1>thi is something new in name </h1>
+
+
+                <h2>this is some new change</h2>
             </div>
         </div>
     )
