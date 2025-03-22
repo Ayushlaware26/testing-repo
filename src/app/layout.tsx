ca88205c27c97f570051cbed5d37b99c43030840
ayush <ayushlaware@gmail.com>
2025-03-23 03:49:47 +0530

    test3

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,12 +27,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-<<<<<<< HEAD
 
         <h1>this is a test aswell</h1>
-=======
         <h1>this is testing</h1>
->>>>>>> ayush
+        <h1>this is test3</h1>
         {children}
       </body>
     </html>
